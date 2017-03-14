b18e5bfa9e0f93ee630dfba0644d3a2be74334b9
beckyfelter <rfelter@vt.edu>
2017-03-14 10:18:20 -0400

    troubleshooting

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
@@ -0,0 +1,6 @@
+fil = new Array();
+fil["0"]= "Creating User Documentation/springbreak/concepts/c-cruise.html@@@Spring break trip 2017@@@We are going on a cruise to the Bahamas...";
+fil["1"]= "Creating User Documentation/springbreak/tasks/t-createaccount.html@@@How to create an account on Carnival.com@@@These steps will guide you in registering for the correct cruise on the Carnival.com website...";
+fil["2"]= "Creating User Documentation/springbreak/tasks/t-selectcruise.html@@@How to select a cruise@@@On the Carnival homepage, you will enter cruise specifics to find the correct cruise to sign up for this spring break...";
+fil["3"]= "Creating User Documentation/springbreak/tasks/t-signupcruise.html@@@How to sign up for an excursion@@@We will be participating in the &quot;Sail and Snorkel&quot; excursion in Nassau, Bahamas.\n        Follow the steps to sign up for the excursion...";
+fil["4"]= "Creating User Documentation/springbreak/tasks/t-traveltoport.html@@@How to travel to the port@@@Here are the steps to traveling to the port in Orlando, Florida to board the cruise.\n        You will drive to the port from Virginia Tech, which is a 10.5 hour drive...";
---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,8 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "Creating User Documentation/springbreak/concepts/c-cruise.html";
+fl["1"]= "Creating User Documentation/springbreak/tasks/t-createaccount.html";
+fl["2"]= "Creating User Documentation/springbreak/tasks/t-selectcruise.html";
+fl["3"]= "Creating User Documentation/springbreak/tasks/t-signupcruise.html";
+fl["4"]= "Creating User Documentation/springbreak/tasks/t-traveltoport.html";
+var doStem = false;searchLoaded = true;
