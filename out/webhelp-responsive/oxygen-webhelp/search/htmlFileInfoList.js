896a6e0fbd4b2d8543eb169ea27eba80b872332b
beckyfelter <rfelter@vt.edu>
2017-03-15 22:23:44 -0400

    new template

--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileInfoList.js
@@ -10,4 +10,4 @@ fil["7"]= "tasks/t-createaccount.html@@@How to create an account on Carnival.com
 fil["8"]= "tasks/t-selectcruise.html@@@How to select a cruise@@@On the Carnival homepage, you will enter cruise specifics to find the correct cruise to sign up for this spring break...";
 fil["9"]= "tasks/t-signupcruise.html@@@How to sign up for an excursion@@@We will be participating in the &quot;Sail and Snorkel&quot; excursion in Nassau, Bahamas.\n        Follow the steps to sign up for the excursion...";
 fil["10"]= "tasks/t-traveltoport.html@@@How to travel to the port@@@Here are the steps to traveling to the port in Orlando, Florida to board the cruise.\n        You will drive to the port from Virginia Tech, which is a 10.5 hour drive...";
-fil["11"]= "tasks/troubleshooting.html@@@Cannot find cruise@@@You searched the website, but you can&apos;t find the cruise specified. The cruise date has passed already. Sign up earlier next year...";
+fil["11"]= "tasks/troubleshooting.html@@@Cannot sign up for excursion@@@The website does not allow you to register for the snorkeling excursion. The online registration date has passed. Contact the customer service team via phone number...";
